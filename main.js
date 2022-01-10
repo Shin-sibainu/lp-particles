9bef14f602fb54bf22aa8cde029d77509302232b
Shin-sibainu <shincode0712@gmail.com>
2022-01-10 21:02:06 +0900

    allmost done

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "lil-gui";
 
 /**
@@ -13,9 +14,7 @@ const gui = new dat.GUI();
 // Canvas
 const canvas = document.querySelector("#webgl");
 
-/**
- * Sizes
- */
+//Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -34,14 +33,80 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 6;
 scene.add(camera);
 
-/**
- * Renderer
- */
+//Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//ドーナツパーティクルジオメトリ
+const donutsParticleGeometry = new THREE.TorusGeometry(2.8, 0.7, 16, 100);
+
+//マテリアル
+const donutParticleMaterial = new THREE.PointsMaterial({
+  size: 0.023,
+  color: "#40ff1a",
+});
+
+//メッシュ
+const donutParticle = new THREE.Points(
+  donutsParticleGeometry,
+  donutParticleMaterial
+);
+
+//パーティクル
+const count = 5000;
+const particleGeometry = new THREE.BufferGeometry();
+const positionArray = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+  positionArray[i] = (Math.random() - 0.5) * 15;
+}
+
+particleGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positionArray, 3)
+);
+
+//マテリアル
+const particleMaterial = new THREE.PointsMaterial({
+  size: 0.01,
+});
+
+//メッシュ
+const particle = new THREE.Points(particleGeometry, particleMaterial);
+
+scene.add(donutParticle, particle);
+
+//debug
+gui.addColor(donutParticleMaterial, "color");
+
+//カメラ制御
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * アニメーション
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  //カメラワーク
+  camera.position.x = Math.cos(elapsedTime * 0.5) * 6;
+  camera.position.z = Math.sin(elapsedTime * 0.5) * 6;
+
+  controls.update();
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
 
 //ブラウザのリサイズ操作
 window.addEventListener("resize", () => {
@@ -57,20 +122,3 @@ window.addEventListener("resize", () => {
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-
-/**
- * アニメーション
- */
-const clock = new THREE.Clock();
-
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
